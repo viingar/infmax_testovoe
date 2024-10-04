0ac7d9965d339559d95329bb7d963bdc0cda5c8b
viingar <myuniverse8082@gmail.com>
2024-10-04 09:32:44 +0300

    final

--- src/main/java/org/example/RetrieveData.java
+++ src/main/java/org/example/RetrieveData.java
@@ -20,10 +20,28 @@ public class RetrieveData {
     private long maxWeight = Long.MIN_VALUE;
     private long minWeight = Long.MAX_VALUE;
 
+    private GroupTypeKey firstKey = null;
+    private GroupTypeKey secondToLastKey = null;
+    private int firstKeyCount = 0;
+    private int secondToLastKeyCount = 0;
+
     public void processData(Data object) {
         GroupTypeKey key = new GroupTypeKey(object.getGroup(), object.getType());
 
+        if (firstKey == null) {
+            firstKey = key;
+        }
+
+        secondToLastKey = key;
+
         duplicates.put(key, duplicates.getOrDefault(key, 0) + 1);
+
+        if (key.equals(firstKey)) {
+            firstKeyCount++;
+        }
+        if (key.equals(secondToLastKey)) {
+            secondToLastKeyCount++;
+        }
 
         long weight = object.getWeight();
         weightsByGroup.put(object.getGroup(),
@@ -39,9 +57,10 @@ public class RetrieveData {
     }
 
     public void printResults() {
-
         boolean hasDuplicates = false;
         System.out.println("Дубликаты объектов:");
+
+
         for (Map.Entry<GroupTypeKey, Integer> entry : duplicates.entrySet()) {
             if (entry.getValue() > 1) {
                 System.out.println("Группа: " + entry.getKey().group + ", Тип: " + entry.getKey().type + " : " + entry.getValue());
@@ -49,15 +68,25 @@ public class RetrieveData {
             }
         }
 
+
+        if (firstKeyCount > 1) {
+            System.out.println("Группа: " + firstKey.group + ", Тип: " + firstKey.type + " : " + firstKeyCount);
+            hasDuplicates = true;
+        }
+
+
+
         if (!hasDuplicates) {
             System.out.println("Нет дубликатов");
         }
+
 
         System.out.println("Суммарный вес объектов в каждой группе:");
         weightsByGroup.forEach((group, totalWeight) -> System.out.println(group + " : " + totalWeight));
 
         System.out.println("Максимальный вес: " + maxWeight);
         System.out.println("Минимальный вес: " + minWeight);
+
 
         clearData();
     }
@@ -67,5 +96,9 @@ public class RetrieveData {
         weightsByGroup.clear();
         maxWeight = Long.MIN_VALUE;
         minWeight = Long.MAX_VALUE;
+        firstKey = null;
+        secondToLastKey = null;
+        firstKeyCount = 0;
+        secondToLastKeyCount = 0;
     }
 }
